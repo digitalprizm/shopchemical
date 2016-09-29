d475d681fd50261967dbf337b5e27f430478df04
erpserver <erp@example.com>
2016-09-29 17:17:12 +0200

    updated reg forms

---
+++ shopchemical/shopchemical/doctype/supplier_registration/test_supplier_registration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, SBK and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Supplier Registration')
+
+class TestSupplierRegistration(unittest.TestCase):
+	pass
