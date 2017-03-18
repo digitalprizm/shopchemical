ae823f1127c282c416747153d701489095b2563d
erpserver <erp@example.com>
2017-03-18 12:02:44 +0100

    updated fixtures and category

---
+++ shopchemical/shopchemical/doctype/get_quote/get_quote.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, SBK and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Get Quote', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ shopchemical/shopchemical/doctype/interaction_master/interaction_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, SBK and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Interaction Master', {
+	refresh: function(frm) {
+
+	}
+});
