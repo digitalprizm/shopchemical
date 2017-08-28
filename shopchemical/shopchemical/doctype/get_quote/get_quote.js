6345a872613256c6ba55578a5da46be7bd43be58
sambhaji <kolate.sambhaji@gmail.com>
2017-08-28 21:49:42 +0530

    added create quote

--- shopchemical/shopchemical/doctype/get_quote/get_quote.js
+++ shopchemical/shopchemical/doctype/get_quote/get_quote.js
@@ -3,6 +3,14 @@
 
 frappe.ui.form.on('Get Quote', {
 	refresh: function(frm) {
-
+		if(frm.doc.docstatus == 1) {
+			cur_frm.add_custom_button(__("Create Quotation"),
+				function() {
+					frappe.model.open_mapped_doc({
+							method: "shopchemical.shopchemical.doctype.get_quote.get_quote.create_quote",
+							frm: cur_frm
+					})
+				})
+			}
 	}
 });
--- shopchemical/shopchemical/doctype/get_quote/get_quote.py
+++ shopchemical/shopchemical/doctype/get_quote/get_quote.py
@@ -4,7 +4,94 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
+
+import json
+from datetime import timedelta
+# from erpnext.controllers.queries import get_match_cond
+from frappe.utils import flt, formatdate, time_diff_in_hours, get_datetime, getdate, today, cint, get_datetime_str,get_time
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
+from frappe.utils import flt, today, getdate
+from email.utils import parseaddr
+from frappe.model.naming import get_default_naming_series
 
 class GetQuote(Document):
 	pass
+
+
+
+
+@frappe.whitelist()
+def create_quote(source_name, target_doc=None):
+	def set_missing_values(source, target):
+		company_name = frappe.db.get_single_value('Global Defaults', 'default_company')
+
+		# company_name=frappe.db.get_value("Company",{"company_name":source.doctor},"company_name")
+		company_cost_center = frappe.db.get_value("Company",{"company_name":company_name},"cost_center")
+		# company_income_account = frappe.db.get_value("Company",{"company_name":source.doctor},"default_income_account")
+		# company_additional_info = frappe.db.get_value("Company",{"company_name":source.company},"additional_info")
+		# customer_name = frappe.db.get_value("Customer",{"customer_name":source.surgeon},"customer_name")
+		# print customer_name
+
+		email_id = source.email_id
+		real_name, email_id = parseaddr(email_id)
+
+		if frappe.db.get_value("Lead", {"email_id": email_id}):
+			lead = frappe.db.get_value("Lead", {"email_id": email_id})
+		else: 
+			lead = frappe.get_doc({
+				"doctype": "Lead",
+				"phone": source.contact_number,
+				"email_id": source.email_id,
+				"lead_name": real_name or source.email_id,
+				"status": "Lead",
+				"naming_series": get_default_naming_series("Lead"),
+				"company": frappe.db.get_default("Company"),
+				"source": "Website"
+			})
+			lead.insert()
+			lead = lead.name
+
+		# if source.is_surgeon_billable==1:
+		# 	target.customer = source.surgeon
+		# else:
+		# 	target.customer = source.hospital
+		# target.doctor = source.doctor
+		# target.surgeon = source.surgeon
+		# target.surgeon_1 = source.surgeon_1
+		# target.surgeon_2 = source.surgeon_2
+		# target.patient = source.patient
+		# target.case = source.name
+		# target.patient_case_no=source.case_no
+		target.company = company_name
+		target.quotation_to = "Lead"
+		target.lead = lead
+		target.due_date = today()
+		target.set('items', [])
+		k = target.append('items', {})
+		# k.name = source.operation_type
+		k.item_code = source.item_name
+		k.item_name = source.item_name
+		# k.description = "Case Fee for Case" + source_name + " and Doctor " + source.doctor + " and Hospial " + source.hospital
+		k.cost_center = company_cost_center
+		# k.income_account = company_income_account
+		# k.additional_info = company_additional_info
+		k.uom = "Nos"
+		k.conversion_factor = 1
+		k.stock_uom = "Nos"
+		k.qty = 1
+		# k.rate = source.rate
+		target.get_quote = source.name
+		
+	doclist = get_mapped_doc("Get Quote", source_name, {
+			"Get Quote": {
+				"doctype": "Quotation",
+				"validation": {
+					"docstatus": ["=", 1]
+				},
+			}
+		}, target_doc, set_missing_values, ignore_permissions=False)
+	return doclist
+
+
